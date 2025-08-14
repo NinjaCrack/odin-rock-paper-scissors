5aa4fb020e32db7f9ae62bc6ff65b6a4dded44c6
NinjaCrack <41764014+NinjaCrack@users.noreply.github.com>
2025-08-15 01:21:16 +0800

    Add game layout with CSS styles, images

--- js/script.js
+++ js/script.js
@@ -1,1 +1,36 @@
-console.log("Hello World");
+// const randomNum = Math.floor(Math.random() * 3) +1;
+// //rock = 1
+// //paper = 2
+// //scissor = 3
+
+
+// if (randomNum == 1) {
+//     console.log("Rock");
+// } else if (randomNum == 2) {
+//     console.log("Paper")
+// } else if (randomNum == 3) {
+//     console.log("Scissors")
+// }
+
+
+let choice;
+function getComputerChoice() {
+    const computerChoice = Math.floor(Math.random() * 3) +1;
+    console.log(computerChoice);
+    
+    if (computerChoice == 1) {
+        choice = "Rock";
+    } else if (computerChoice == 2) {
+        choice = "Paper";
+    } else if (computerChoice == 3) {
+        choice = "Scissors";
+    }
+
+    return choice;
+}
+
+function getHumanChoice() {
+    pass
+}
+getComputerChoice();
+console.log(choice);
