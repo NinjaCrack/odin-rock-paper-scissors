7372d79febbde1141f2a0b44c0b52d025fbb07d0
NinjaCrack <41764014+NinjaCrack@users.noreply.github.com>
2025-08-15 16:08:07 +0800

    Enhance game functionality and styling: add score tracking, update CSS for better visuals, and implement game over logic

--- js/script.js
+++ js/script.js
@@ -1,36 +1,88 @@
-// const randomNum = Math.floor(Math.random() * 3) +1;
-// //rock = 1
-// //paper = 2
-// //scissor = 3
+let randomChoice = Math.floor(Math.random() * 3) +1;
+const rockbtn = document.getElementById("rock");
+const paperbtn = document.getElementById("paper");
+const scissorsbtn = document.getElementById("scissors");
+const resultOutput = document.getElementById('result');
+const yourScore = document.getElementById('yourScore');
+const compScore = document.getElementById('computerScore');
+const gameNumber = document.getElementById('gameNumber');
+
+let gameCounter = 0;
+let computerChoice;
+let humanChoice;
+let humanScore = 0;
+let computerScore = 0;
 
 
-// if (randomNum == 1) {
-//     console.log("Rock");
-// } else if (randomNum == 2) {
-//     console.log("Paper")
-// } else if (randomNum == 3) {
-//     console.log("Scissors")
-// }
-
-
-let choice;
-function getComputerChoice() {
-    const computerChoice = Math.floor(Math.random() * 3) +1;
-    console.log(computerChoice);
-    
-    if (computerChoice == 1) {
-        choice = "Rock";
-    } else if (computerChoice == 2) {
-        choice = "Paper";
-    } else if (computerChoice == 3) {
-        choice = "Scissors";
+function playRound(humanChoice, computerChoice) {
+    if(computerChoice === humanChoice) {
+        resultOutput.textContent = `Its a Draw! You both chose ${humanChoice}`;
+    } else if(
+        (humanChoice === "Rock" && computerChoice === "Scissors") ||
+        (humanChoice === "Paper" && computerChoice === "Rock") ||
+        (humanChoice === "Scissors" && computerChoice === "Paper")
+    ) { 
+        resultOutput.textContent = `You Win! ${humanChoice} beats ${computerChoice}!`;
+        humanScore += 1;
+        yourScore.textContent = humanScore;
+        
+    } else {
+        resultOutput.textContent = `You Lose! ${computerChoice} beats ${humanChoice}!`
+        computerScore += 1;
+        compScore.textContent = computerScore;
     }
+    randomChoice = Math.floor(Math.random() * 3) +1;
+    gameCounter++;
+    gameNumber.textContent = `${gameCounter} / 10`;
 
-    return choice;
+     if( gameCounter >= 10) {
+        if(humanScore > computerScore) {
+            resultOutput.textContent = 'Game Over! You Win!';
+            
+        } else if(humanScore == computerScore) {
+            resultOutput.textContent = 'Game Over! Draw!'
+        }
+        else {
+            resultOutput.textContent = 'Game Over! You Computer Wins!'
+        }
+
+        rockbtn.disabled = true;
+        paperbtn.disabled = true;
+        scissorsbtn.disabled = true;
+        resultOutput.innerHTML += `<br> <a href="#" class="reset" onclick="location.reload(); return false;">Play Again</a>`;
+     }
+
 }
 
-function getHumanChoice() {
-    pass
+function getComputerChoice() {
+    if( randomChoice == 1) {
+        computerChoice = "Rock";
+    } else if(randomChoice == 2) {
+        computerChoice = "Paper";
+    } else if(randomChoice == 3) {
+        computerChoice = "Scissors";
+    }
+    return computerChoice;
 }
-getComputerChoice();
-console.log(choice);
+
+function getHumanChoice(){
+    rockbtn.addEventListener('click', function() {
+        humanChoice = this.value;
+        computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    });
+
+    paperbtn.addEventListener('click', function() {
+        humanChoice = this.value;
+        computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    });
+    scissorsbtn.addEventListener('click', function() {
+        humanChoice = this.value;
+        computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    });
+}
+
+getHumanChoice();
+
